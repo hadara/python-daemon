91f051b5d73d7a662a666891fe1d0bd5c1c91454
PyYoshi <myoshi321go@gmail.com>
2013-05-08 21:00:43 +0900

    add .gitignore and version_info.py

---
+++ daemon/version/version_info.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+version_info = {'branch_nick': u'python-daemon.devel',
+ 'build_date': '2010-03-10 10:00:26 +1100',
+ 'clean': None,
+ 'date': '2010-03-10 10:00:26 +1100',
+ 'revision_id': '',
+ 'revno': 325}
+
+revisions = {}
+
+file_revisions = {}
